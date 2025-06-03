d16287835e59ffa0c8f10272ef0075cb3064c33e
wizard supreme <smn7818@gmail.com>
2025-06-03 08:49:31 -0400

    added tests but failing

--- packages/nx-surrealdb-migrations/jest.config.ts
+++ packages/nx-surrealdb-migrations/jest.config.ts
@@ -1,10 +1,28 @@
+/* eslint-disable */
 export default {
   displayName: 'nx-surrealdb-migrations',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+    '^.+\\.[tj]s$': ['ts-jest', {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      useESM: true
+    }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/packages/nx-surrealdb-migrations',
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/files/**',
+    '!jest.config.ts',
+  ],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  testEnvironmentOptions: {
+    node: {
+      experimentalVMModules: true
+    }
+  }
 };
